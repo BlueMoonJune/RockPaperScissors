6de554e341a7de2f1af393d489591fef0dcec95c
unknown <3SpencerW@gmail.com>
2024-10-03 09:06:54 -0700

    Loop and overall results added!!! :D

--- main.py
+++ main.py
@@ -9,49 +9,55 @@ options = {
     "paper": 1,
     "scissors": 2
 }
+#start loop here
+while max(scores) < 10:
+    # Generate computer's choice
+    computer = randint(0,2)
 
-# Generate computer's choice
-computer = randint(0,2)
+    #Take user input
+    user = -1
+    while True:
+        userOption = input("Rock, Paper, or Scissors?").lower()
+        #turn input into integer id
+        if userOption in options:
+            user = options[userOption]
+            break
+        else:
+            print("invalid input")
 
-#Take user input
-user = -1
-while True:
-    userOption = input("Rock, Paper, or Scissors?").lower()
-    #turn input into integer id
-    if userOption in options:
-        user = options[userOption]
-        break
-    else:
-        print("invalid input")
+    #print(user)
 
-print(user)
+    #See who wins
 
-#See who wins
+    dif = (computer - user) % 3 #0: Draw, 1: computer, 2: user
 
-dif = (computer - user) % 3 #0: Draw, 1: computer, 2: user
+    scores[dif] += 1
 
-scores[dif] += 1
+    #Evaluate a draw
+    if dif == 0:
+        print("Its a draw!")
 
-#Evaluate a draw
-if dif == 0:
-    print("Its a draw!")
+    #Check for scissors
+    if user == 2 and computer == 1:
+        print("Scissors beats paper, you win!")
+    elif user == 1 and computer == 2:
+        print("Scissors beats paper, you lose!")
 
-#Check for scissors
-if user == 2 and computer == 1:
-    print("Scissors beats paper, you win!")
-elif user == 1 and computer == 2:
-    print("Scissors beats paper, you lose!")
+    #Check for rock
+    if user == 0 and computer == 2:
+        print("Rock beats scissors, you win!")
+    elif user == 2 and computer == 0:
+        print("Rock beats scissors, you lose!")
 
-#Check for rock
-if user == 0 and computer == 2:
-    print("Rock beats scissors, you win!")
-elif user == 2 and computer == 0:
-    print("Rock beats scissors, you lose!")
-
-#Check for paper
-if user == 1 and computer == 0:
-    print("Paper beats Rock, you win!")
-elif user == 0 and computer == 1:
-    print("Paper beats Rock, you lose!")
-
+    #Check for paper
+    if user == 1 and computer == 0:
+        print("Paper beats Rock, you win!")
+    elif user == 0 and computer == 1:
+        print("Paper beats Rock, you lose!")
+if scores[0] == 10:
+    print("Overall draw!")
+if scores[1] == 10:
+    print("Computer overall win!!!")
+if scores[2] == 10:
+    print("User overall win!!!")
 
